30ca1cc438e6b019afa852823a0f716495cc9777
TANVIR-AHMED-7618 <tanvir.pstu.web@gmail.com>
2024-05-20 09:55:54 +0600

    import function from views

--- django_pro/django_pro/urls.py
+++ django_pro/django_pro/urls.py
@@ -1,27 +1,15 @@
-"""
-URL configuration for django_pro project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
 from django.contrib import admin
 from django.urls import path
-from machine_learning import views
-from Blogs import views as fb
+from machine_learning.views import machine
+from machine_learning.views import deep_learning
+from Blogs.views import first_blog
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.machine),
-    path('deep/', views.deep_learning),
-    path('fb/', fb.first_blog),
+    path('', machine),
+    path('deep/', deep_learning),
+    path('fb/',first_blog),
 ]
