60666e396d28da347d335a712587cbe06a9cadd5
TANVIR-AHMED-7618 <tanvir.pstu.web@gmail.com>
2024-05-23 12:03:24 +0600

    ..

--- django_pro/django_pro/urls.py
+++ django_pro/django_pro/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ml/', include('machine_learning.urls')),
+    path('', include('machine_learning.urls')),
     path('dl/', include('Deep_learning.urls')),
     path('da/', include('data_analysis.urls')),
     path('Blg/', include('Blogs.urls')),
--- django_pro/machine_learning/urls.py
+++ django_pro/machine_learning/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 
 urlpatterns = [
-    path('mach/', views.machine),
+    path('', views.machine),
 ]
